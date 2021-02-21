bbbe0d423453d40a92812877842f11d9d5af3a71
Jérémie <jeremielegrand021@gmail.com>
2021-02-21 19:12:40 +0100

    [USER] logout function

--- api/src/Controller/SecurityController.php
+++ api/src/Controller/SecurityController.php
@@ -4,15 +4,14 @@ namespace App\Controller;
 use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends AbstractController{
 
     /**
-     * 
      * @Route("/login", name="api_login", methods={"POST"} )
-     * 
      */
-    public function login(IriConverterInterface $iriConverterInterface) {
+    public function login(IriConverterInterface $iriConverter) {
 
         if(!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
@@ -23,6 +22,14 @@ class SecurityController extends AbstractController{
         return new Response(null, 204, [
             'Location' => $iriConverter->getIriFromItem($this->getUser())
         ]);
+    }
+
+     /**
+     * @Route("/logout", name="api_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('should not be reached');
     }
 
 }
